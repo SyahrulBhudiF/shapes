2db78c22f43ff2b0268740af496b33fe5b9979ac
SyahrulBhudiF <syahrul4w@gmail.com>
2024-09-13 06:56:44 +0700

    fix layout

--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -2,15 +2,14 @@
 
 import React from "react";
 
-export function Layout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <div className="flex-1 flex flex-col justify-center items-center p-8 pt-6">
-      {children}
-    </div>
-  );
+export default function Layout({
+                                   children,
+                               }: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className="flex-1 flex flex-col justify-center items-center p-8 pt-6">
+            {children}
+        </div>
+    );
 }
-export default Layout;
