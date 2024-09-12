fcc9b5de5e24f319104d6932bccfc918152720ca
SyahrulBhudiF <syahrul4w@gmail.com>
2024-09-13 01:25:31 +0700

    fix: add min to schema

--- src/lib/schema/userSchema.ts
+++ src/lib/schema/userSchema.ts
@@ -2,11 +2,11 @@ import {z} from "zod";
 
 // schema for validation at form
 const userSchema = z.object({
-    name: z.string(),
-    schoolName: z.string(),
-    age: z.number(),
-    address: z.string(),
-    phone: z.string(),
+    name: z.string().min(5),
+    schoolName: z.string().min(5),
+    age: z.number().min(5),
+    address: z.string().min(5),
+    phone: z.string().min(5),
 })
 
 export default userSchema;
